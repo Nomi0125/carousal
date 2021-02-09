fb25f6ea07c61efcef2b435c4a1c6e4729784d7e
unknown <nomansandhu0125@gmail.com>
2021-02-09 20:29:24 +0500

    cleaning the comments

--- app.js
+++ app.js
@@ -23,31 +23,23 @@
 // ---------------------manual------------
 var slideIndex = 1;
 showSlides(slideIndex)
-// setInterval(, 1000)
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
 
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
 
 function showSlides(n) {
-  console.log('chal raha hai')
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  // var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-      // console.log(slides[i])
+    slides[i].style.display = "none";  
   }
-  // for (i = 0; i < dots.length; i++) {
-  //     dots[i].className = dots[i].className.replace(" active", "");
-  // }
   slides[slideIndex-1].style.display = "block";  
-  // dots[slideIndex-1].className += " active";
-  // setInterval(showSlides(slideIndex), 1000)
 }
+
+setInterval(function plusSlides() {
+  showSlides(slideIndex += 1);
+}, 3000)
